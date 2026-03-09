142ad7be237fa07792716a742e0c60bd7678a667
tildaamelia <tildaamelia@gmail.com>
2026-03-09 16:22:56 +0200

    Height and weight setting inputs updated

--- RPi/wserver.py
+++ RPi/wserver.py
@@ -2,12 +2,21 @@ from flask import Flask
 from flask import render_template
 from flask import jsonify
 from flask import Response
+from flask import request
 
 import db
 import hike
+import os
+import json
 
 app = Flask(__name__)
 hdb = db.HubDatabase()
+
+with open("settings.json") as f: 
+    settings = json.load(f) 
+    
+hike.USER_HEIGHT = settings["height"] 
+hike.USER_WEIGHT = settings["weight"]
 
 @app.route('/')
 def get_home():
@@ -41,6 +50,34 @@ def delete_session(id):
     hdb.delete(id)
     print(f'DELETED SESSION WITH ID: {id}')
     return Response(status=202)
+
+@app.route('/settings')
+def get_settings():
+    return jsonify({
+        "height": hike.USER_HEIGHT,
+        "weight": hike.USER_WEIGHT
+    })
+
+@app.route('/save-settings', methods=['POST'])
+def save_settings():
+
+    data = request.get_json()
+
+    height = int(data['height'])
+    weight = int(data['weight'])
+
+    hike.USER_HEIGHT = height
+    hike.USER_WEIGHT = weight
+
+    settings = {
+        "height": height,
+        "weight": weight
+    }
+
+    with open("settings.json", "w") as f:
+        json.dump(settings, f)
+
+    return jsonify(settings)
 
 if __name__ == "__main__":
     app.run('0.0.0.0', debug=True)
