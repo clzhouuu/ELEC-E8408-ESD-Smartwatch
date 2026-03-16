f5fe315684392ebd3c5a64e7da8a8bc37bef623d
tildaamelia <tildaamelia@gmail.com>
2026-03-16 16:31:59 +0200

    bluetooth check fixed

--- RPi/wserver.py
+++ RPi/wserver.py
@@ -20,7 +20,7 @@ if metrics:
 @app.route('/')
 def get_home():
     sessions = hdb.get_sessions() 
-    return render_template('home.html', sessions=sessions, bt_connected=hub_bt.connected)
+    return render_template('home.html', sessions=sessions, bt_connected=hubbt.connected)
 
 @app.route('/session-page')
 def get_session():
