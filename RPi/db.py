a75c8ef7ffb50f1574d53f4935467570555e83fe
clzhouuu <christinalinzhou@gmail.com>
2026-03-16 17:28:49 +0200

    added date, start time and duration to web UI

--- RPi/db.py
+++ RPi/db.py
@@ -12,6 +12,7 @@ DB_SESSION_TABLE = {
         "km REAL",
         "steps integer",
         "burnt_kcal integer",
+        "date integer",
         "start_time integer",
         "duration integer"
     ]
@@ -80,7 +81,7 @@ class HubDatabase:
 
             try:
                 self.cur.execute(
-                    f"INSERT INTO {DB_SESSION_TABLE['name']} VALUES (?, ?, ?, ?)", (s.id, s.km, s.steps, s.kcal))   
+                    f"INSERT INTO {DB_SESSION_TABLE['name']} VALUES (?, ?, ?, ?, ?, ?, ?)", (s.id, s.km, s.steps, s.kcal, s.date, s.start_time, s.duration))
                 for lat, lon, alt in s.coords:
                     self.cur.execute(
                         f"INSERT INTO {DB_GPS_TABLE['name']} (session_id, lat, lon, alt) VALUES (?, ?, ?, ?)",(s.id, lat, lon, alt))         
--- RPi/hike.py
+++ RPi/hike.py
@@ -9,23 +9,20 @@ class HikeSession:
     km = 0
     steps = 0
     kcal = -1
-    start_time = 0
-    end_time = 0
-    duration = 0
+    date = ''
+    start_time = ''
+    duration = ''
     coords = []
 
     # represents a computationally intensive calculation done by lazy execution.
     def calc_kcal(self):
         self.kcal = MET_HIKING * KCAL_PER_STEP * self.steps
 
-    def calc_dur(self):
-        self.duration = self.end_time - self.start_time
-
     def __repr__(self):
         return f"HikeSession{{{self.id}, {self.km}(km), {self.steps}(steps), {self.kcal:.2f}(kcal), {self.start_time}(start_time), {self.duration}(duration), {self.coords}(coords)}}"
 
 def to_list(s: HikeSession) -> list:
-    return [s.id, s.km, s.steps, s.kcal, s.start_time, s.duration]
+    return [s.id, s.km, s.steps, s.kcal, s.date, s.start_time, s.duration]
 
 def from_list(l: list) -> HikeSession:
     s = HikeSession()
@@ -33,6 +30,7 @@ def from_list(l: list) -> HikeSession:
     s.km = l[1]
     s.steps = l[2]
     s.kcal = l[3]
-    s.start_time = l[4]
-    s.duration = l[5]
+    s.date = l[4]
+    s.start_time = l[5]
+    s.duration = l[6]
     return s
