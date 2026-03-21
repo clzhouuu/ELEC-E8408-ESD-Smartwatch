01b21c2ec9fce8865378ddbc3b13618017718be5
tildaamelia <tildaamelia@gmail.com>
2026-03-21 19:26:19 +0200

    main.py starts both bluetooth receiver and UI

--- RPi/main.py
+++ RPi/main.py
@@ -1,24 +1,7 @@
 import threading
-import wserver
 import receiver
+import wserver
 
-def main():
-    print("Starting Bluetooth receiver.")
-    try:
-        while True:
-            hubbt.wait_for_connection()
-            hubbt.synchronize(callback=process_sessions)
-            
-    except KeyboardInterrupt:
-        print("CTRL+C Pressed. Shutting down the server...")
-
-    except Exception as e:
-        print(f"Unexpected shutdown...")
-        print(f"ERROR: {e}")
-        hubbt.sock.close()
-        raise e
-
-thread = threading.Thread(target=main, daemon=True)
+thread = threading.Thread(target=receiver.main, daemon=True)
 thread.start()
-
 wserver.app.run('0.0.0.0', debug=True)
